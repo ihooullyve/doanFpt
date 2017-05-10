51f67fd42b6ed37659038474f5f196e04a8f48e6
pacificsoftdevld <pacificsoftdev.ld@gmail.com>
2017-05-10 11:56:20 +0700

    update

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,20 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    # sinh vien
+    url(r'^sinhvien/them$', views.sinhvien_them, name="sinhvien_them"),
+    url(r'^sinhvien/chinhsua$', views.sinhvien_sua, name="sinhvien_sua"),
+    url(r'^sinhvien/timkiem$', views.sinhvien_timkiem, name="sinhvien_timkiem"),
+    # detai
+    url(r'^detai/them$', views.detai_them, name="detai_them"),
+    url(r'^detai/chinhsua$', views.detai_sua, name="detai_sua"),
+    url(r'^detai/timkiem$', views.detai_timkiem, name="detai_timkiem"),
+    # giang vien
+    url(r'^giangvien/them$', views.giangvien_them, name="giangvien_them"),
+    url(r'^giangvien/chinhsua$', views.giangvien_sua, name="giangvien_sua"),
+    url(r'^giangvien/timkiem$', views.giangvien_timkiem, name="giangvien_timkiem"),
+    # hoi dong
+    url(r'^hoidong/them$', views.hoidong_them, name="hoidong_them"),
+    url(r'^hoidong/chinhsua$', views.hoidong_sua, name="hoidong_sua"),
+    url(r'^hoidong/timkiem$', views.hoidong_timkiem, name="hoidong_timkiem"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
 
-def index(request):
-	return render(request, 'index.html')
 
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def sinhvien_them(request):
+    return render(request, 'sinhvien/add-new.html')
+
+
+def sinhvien_sua(request):
+    return render(request, 'sinhvien/edit.html')
+
+
+def sinhvien_timkiem(request):
+    return render(request, 'sinhvien/search.html')
+
+
+def detai_them(request):
+    return render(request, 'detai/add-new.html')
+
+
+def detai_sua(request):
+    return render(request, 'detai/edit.html')
+
+
+def detai_timkiem(request):
+    return render(request, 'detai/search.html')
+
+
+def giangvien_them(request):
+    return render(request, 'giangvien/add-new.html')
+
+
+def giangvien_sua(request):
+    return render(request, 'giangvien/edit.html')
+
+
+def giangvien_timkiem(request):
+    return render(request, 'giangvien/search.html')
+
+
+def hoidong_them(request):
+    return render(request, 'hoidong/add-new.html')
+
+
+def hoidong_sua(request):
+    return render(request, 'hoidong/edit.html')
+
+
+def hoidong_timkiem(request):
+    return render(request, 'hoidong/search.html')
